2b8293042b9d5067da37bd85a53e150ebcca97d1
Tarre <tarreng@gmail.com>
2023-11-20 11:49:40 +0100

    Moving code from psvm to separate method.

--- src/Server/QuizkampenServer.java
+++ src/Server/QuizkampenServer.java
@@ -1,26 +1,32 @@
 package Server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 public class QuizkampenServer {
-        public static void main(String[] args) throws Exception {
-            ServerSocket listener = new ServerSocket(55555);
-            System.out.println("Quizkampen Server is Running");
-            try {
-                while (true) {
-                    ServerSidePlayer player1
-                            = new ServerSidePlayer(listener.accept(), "1");
-                    ServerSidePlayer player2
-                            = new ServerSidePlayer(listener.accept(), "2");
 
-                    player1.setOpponent(player2);
-                    player2.setOpponent(player1);
-                    //game.currentPlayer = playerX;
-                    player1.start();
-                    player2.start();
+    public QuizkampenServer() throws IOException {
+        ServerSocket listener = new ServerSocket(55555);
+        System.out.println("Quizkampen Server is Running");
+        try {
+            while (true) {
+                ServerSidePlayer player1
+                        = new ServerSidePlayer(listener.accept(), "1");
+                ServerSidePlayer player2
+                        = new ServerSidePlayer(listener.accept(), "2");
 
-                }
-            } finally {
-                listener.close();
+                player1.setOpponent(player2);
+                player2.setOpponent(player1);
+                //game.currentPlayer = playerX;
+                player1.start();
+                player2.start();
+
             }
+        } finally {
+            listener.close();
+        }
+    }
+
+        public static void main(String[] args) throws Exception {
+            QuizkampenServer qs = new QuizkampenServer();
         }
     }

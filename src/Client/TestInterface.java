8f31c56ecc992d60e7668f40e4817595a554862e
WilliamWisten <william.wisten@yh.nackademin.se>
2023-11-20 13:19:44 +0100

    small change

--- src/Client/TestInterface.java
+++ src/Client/TestInterface.java
@@ -7,7 +7,6 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class TestInterface extends JFrame {
 

89440e1b4522755f6a3f6be3c2a25e344aeb0dae
Yacoub sarsam <Yacoub.sarsam@gmail.com>
2023-11-22 20:44:07 +0100

    impliment basic server-clien kommunication

--- src/Client/QuizkampenClient.java
+++ src/Client/QuizkampenClient.java
@@ -4,8 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 
 public class QuizkampenClient extends JFrame {
+
+    String inputFromServer;
 
     JPanel jp = new JPanel(new BorderLayout());
     JPanel categoryPanel = new JPanel(new GridLayout(2, 1));
@@ -14,10 +17,13 @@ public class QuizkampenClient extends JFrame {
     JLabel questionNumber = new JLabel("Fråga 1");
     JLabel category = new JLabel("Musik");
     JLabel question = new JLabel("Vem sjunger bäst?");
+    JLabel playerStatus = new JLabel();
+
     JButton jb1 = new JButton("Svar 1");
     JButton jb2 = new JButton("Svar 2");
     JButton jb3 = new JButton("Svar 3");
     JButton jb4 = new JButton("Svar 4");
+
 
     public QuizkampenClient() {
 
@@ -30,9 +36,11 @@ public class QuizkampenClient extends JFrame {
         {
 
             this.add(jp);
+
             jp.add(categoryPanel, BorderLayout.NORTH);
             jp.add(questionPanel, BorderLayout.CENTER);
             jp.add(answerPanel, BorderLayout.SOUTH);
+            categoryPanel.add(playerStatus);
             categoryPanel.add(questionNumber);
             categoryPanel.add(category);
             questionPanel.add(question);
@@ -46,12 +54,32 @@ public class QuizkampenClient extends JFrame {
             setVisible(true);
             setLocationRelativeTo(null);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
+            //spelare1
+            inputFromServer = serverIn.readLine();
+            if(inputFromServer.startsWith("1"))
+            {
+                System.out.println("111111111111");
+                System.out.println(inputFromServer);
+                playerStatus.setText(inputFromServer);}
+            //spelare2
+            else if(inputFromServer.startsWith("2")){
+                System.out.println("22222");
+                System.out.println(inputFromServer);
+                playerStatus.setText(inputFromServer);
+            }
+            while (true){
+                inputFromServer = serverIn.readLine();
+                if(Objects.equals(inputFromServer, "Båda är online")){
+                    playerStatus.setText("Båda är online");
+                }
+            }
 
         }
         catch (IOException e){
             e.printStackTrace();
         }
     }
+
 
     public static void main(String[] args) {
         QuizkampenClient qc = new QuizkampenClient();

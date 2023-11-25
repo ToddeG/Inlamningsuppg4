7461027229199b9b2ea292b921f3d7705ba7dae1
WilliamWisten <william.wisten@yh.nackademin.se>
2023-11-25 18:30:36 +0100

    -Changed header on scoreboard -Fixed so text fits frame -Added win/lose text

--- src/Client/QuizkampenClient.java
+++ src/Client/QuizkampenClient.java
@@ -16,6 +16,7 @@ public class QuizkampenClient extends JFrame {
     private BufferedReader serverIn;
     private ObjectInputStream serverInObject;
     private String player1or2;
+    private String[] scoreboardHeader = new String[5];
 
 
     public QuizkampenClient() throws IOException {
@@ -25,10 +26,14 @@ public class QuizkampenClient extends JFrame {
         out1 = new ObjectOutputStream(s.getOutputStream());
         serverIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
         serverInObject = new ObjectInputStream(s.getInputStream());
+        scoreboardHeader[0] = "Väntar på att motståndaren ska spela klart";
+        scoreboardHeader[1] = "Din tur att spela";
+        scoreboardHeader[2] = "Du vann";
+        scoreboardHeader[3] = "Du förlorade";
+        scoreboardHeader[4] = "Det blev lika";
     }
 
     public void play() throws IOException, ClassNotFoundException, InterruptedException {
-        String command;
         Interface client = new Interface();
         boolean firstRound = true;
         boolean lastPlayerRound = false;
@@ -42,12 +47,12 @@ public class QuizkampenClient extends JFrame {
                 out1.writeObject(client.loadQuestionRound((ArrayList<QuestionObject>) serverInObject.readObject()));
                 //Receives score from server, loads GUI with scoreboard, sets firstRound as false
                 GameScore gameScore = (GameScore) serverInObject.readObject();
-                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score());
+                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score(), scoreboardHeader[0]);
                 firstRound = false;
             }
             else if (firstRound && player1or2.equals("2")) { //First round for player 2, only loads scoreboard
                 GameScore gameScore = (GameScore) serverInObject.readObject();
-                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score());
+                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score(), scoreboardHeader[0]);
                 firstRound = false;
             }
             else if (lastPlayerRound) { //Last playing round
@@ -55,12 +60,12 @@ public class QuizkampenClient extends JFrame {
                 out1.writeObject(client.loadQuestionRound((ArrayList<QuestionObject>) serverInObject.readObject()));
                 //Loads scoreboard
                 GameScore gameScore = (GameScore) serverInObject.readObject();
-                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score());
+                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score(), checkIfWon(gameScore));
             }
             else if (lastRoundOpponent){ //Last round for the player that didn't play the last round
                 //Only loads scoreboard
                 GameScore gameScore = (GameScore) serverInObject.readObject();
-                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score());
+                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score(), checkIfWon(gameScore));
             }
             else { //Every round that isn't the first or last
                 //Answer the questions the opponent just answered
@@ -71,7 +76,7 @@ public class QuizkampenClient extends JFrame {
                 out1.writeObject(client.loadQuestionRound((ArrayList<QuestionObject>) serverInObject.readObject()));
                 //Load scoreboard
                 GameScore gameScore = (GameScore) serverInObject.readObject();
-                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score());
+                client.loadScoreboard(gameScore.getPlayer1Score(), gameScore.getPlayer2Score(), scoreboardHeader[0]);
                 if((player1or2.equals("1") && gameScore.getPlayer1Score()[gameScore.getPlayer1Score().length - 1][0] != null) //Checks if the opponent is about to play the last round
                         || (player1or2.equals("2") && gameScore.getPlayer2Score()[gameScore.getPlayer2Score().length - 1][0] != null)){
                     lastRoundOpponent = true;
@@ -91,5 +96,19 @@ public class QuizkampenClient extends JFrame {
 
     public void setPlayer(String number) {
         player1or2 = number;
+    }
+
+    public String checkIfWon(GameScore gameScore){
+        if ((player1or2.equals("1") && gameScore.getPlayer1ScoreNumber() > gameScore.getPlayer2ScoreNumber()) ||
+                (player1or2.equals("2") && gameScore.getPlayer2ScoreNumber() > gameScore.getPlayer1ScoreNumber())){
+            return scoreboardHeader[2];
+        }
+        else if((player1or2.equals("1") && gameScore.getPlayer1ScoreNumber() < gameScore.getPlayer2ScoreNumber()) ||
+                (player1or2.equals("2") && gameScore.getPlayer2ScoreNumber() < gameScore.getPlayer1ScoreNumber())){
+            return scoreboardHeader[3];
+        }
+        else {
+            return scoreboardHeader[4];
+        }
     }
 }
--- src/POJOs/GameScore.java
+++ src/POJOs/GameScore.java
@@ -5,16 +5,41 @@ import java.io.Serializable;
 public class GameScore implements Serializable {
     private Boolean[][] player1Score;
     private Boolean[][] player2Score;
+    private int player1ScoreNumber;
+    private int player2ScoreNumber;
     public GameScore(Boolean[][] player1Score, Boolean[][] player2Score){
         this.player1Score = player1Score;
         this.player2Score = player2Score;
+        player1ScoreNumber = countScore(player1Score);
+        player2ScoreNumber = countScore(player2Score);
     }
 
+    public int countScore(Boolean[][] playerScore) {
+        int score = 0;
+        for (Boolean[] booleans : playerScore) {
+            for (Boolean aBoolean : booleans) {
+                try {
+                    if (aBoolean) {
+                        score++;
+                    }
+                } catch (NullPointerException e) {
+                    return score;
+                }
+            }
+        }
+        return score;
+    }
     public Boolean[][] getPlayer1Score() {
         return player1Score;
     }
 
     public Boolean[][] getPlayer2Score() {
         return player2Score;
+    }
+    public int getPlayer1ScoreNumber(){
+        return player1ScoreNumber;
+    }
+    public int getPlayer2ScoreNumber(){
+        return player2ScoreNumber;
     }
 }
